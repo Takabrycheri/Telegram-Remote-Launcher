bb258e606cff9b9a5349e083b2a3abb43b8517ee
Takabrycheri <takabrycheri@gmail.com>
2020-04-28 02:18:07 +0200

    Added user check

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ except ImportError:
     pip.main(["install", "python-telegram-bot"])
     
     print("--------------------------------------")
-    print("Module installed, bot started!")
+    print("Module installed, bot starting...")
 
     import telegram
     from telegram.ext import Updater
@@ -28,8 +28,16 @@ except ImportError:
     import settings
 
 if settings.token == '':
+    print("--------------------------------------")
     print("Insert your BotFather token in settings.py!")
+    print("--------------------------------------")
     quit()
+
+if settings.user_id == '':
+    print("--------------------------------------")
+    print("Insert your user id in settings.py")
+    print("If you don't know it send a message to bot, it will reply with your user id")
+    print("--------------------------------------")
 
 updater = Updater(token=settings.token, use_context=True)
 dispatcher = updater.dispatcher
@@ -38,53 +46,75 @@ print("Bot online!")
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def start(update, context):
-    context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Hello there :D")
+def checkUser(update, context):
+    user = update.message.from_user
+    if settings.user_id == '':
+        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<i>Hi %s, before you can do any command you must enter your user id in the settings.py file to prevent strangers from sending commands to your PC!</i>\n\n<b>Here is your user ID:</b> <code>%s</code>\n<i>(click it to copy)</i>" % (user['first_name'], user['id']))
+        reason = "auto_shutdown"
+        threading.Thread(target=shutdown, args=(context, update, reason)).start()
+        return False
+    elif str(user['id']) != settings.user_id:
+        return False
+    else:
+        return True
 
-def shutdown():
+def start(update, context):
+    if checkUser(update, context) is True:
+        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Hello there :D")
+
+def shutdown(context, update, reason):
+    if reason == "cmd_stop":
+        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Bot turned off!")
+    if reason == "auto_shutdown":
+        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="I am automatically turning off to allow you to make the change :)")
+
     updater.stop()
     updater.is_idle = False
     print("The bot has been shut down!")
     quit()
 
 def stop(update, context):
-    context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Bot turned off!")
-    threading.Thread(target=shutdown).start()
+    if checkUser(update, context) is True:
+        reason = "cmd_stop"
+        threading.Thread(target=shutdown, args=(context, update, reason)).start()
 
 def open(update, context):
-    file_name = ' '.join(context.args)
-    file_name_c = file_name.capitalize()
-    if file_name in settings.files:
-        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Opening <b>%s</b>" % file_name_c)
-        try:
-            exec("os.startfile(settings.path_%s)" % file_name)
-            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> opened" % file_name_c)
-            print("Opened %s" % file_name_c)
-        except Exception:
-            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> has failed to open :(" % file_name_c)
-            print("Failed to open %s" % file_name_c)
-    else:
-        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> is not defined in settings.py" % file_name_c)
-        print("Tried to open %s, but is not defined in settings.py" % file_name_c)
+    if checkUser(update, context) is True:
+        file_name = ' '.join(context.args)
+        file_name_c = file_name.capitalize()
+        if file_name in settings.files:
+            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Opening <b>%s</b>" % file_name_c)
+            try:
+                exec("os.startfile(settings.path_%s)" % file_name)
+                context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> opened" % file_name_c)
+                print("Opened %s" % file_name_c)
+            except Exception:
+                context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> has failed to open :(" % file_name_c)
+                print("Failed to open %s" % file_name_c)
+        else:
+            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> is not defined in settings.py" % file_name_c)
+            print("Tried to open %s, but is not defined in settings.py" % file_name_c)
 
 def launch(update, context):
-    software_name = ' '.join(context.args)
-    software_name_c = software_name.capitalize()
-    if software_name in settings.softwares:
-        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Launching <b>%s</b>" % software_name_c)
-        try:
-            exec("os.startfile(settings.path_%s)" % software_name)
-            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> launched :)" % software_name_c)
-            print("Launched %s" % software_name_c)
-        except Exception:
-            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> has failed to launch :(" % software_name_c)
-            print("Failed to launch %s" % software_name_c)
-    else:
-        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> is not defined in settings.py" % software_name_c)
-        print("Tried to launch %s, but is not defined in settings.py" % software_name_c)
+    if checkUser(update, context) is True:
+        software_name = ' '.join(context.args)
+        software_name_c = software_name.capitalize()
+        if software_name in settings.softwares:
+            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Launching <b>%s</b>" % software_name_c)
+            try:
+                exec("os.startfile(settings.path_%s)" % software_name)
+                context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> launched :)" % software_name_c)
+                print("Launched %s" % software_name_c)
+            except Exception:
+                context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> has failed to launch :(" % software_name_c)
+                print("Failed to launch %s" % software_name_c)
+        else:
+            context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="<b>%s</b> is not defined in settings.py" % software_name_c)
+            print("Tried to launch %s, but is not defined in settings.py" % software_name_c)
 
 def unknown(update, context):
-    context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Unkown command :(")
+    if checkUser(update, context) is True:
+        context.bot.send_message(parse_mode="HTML", chat_id=update.effective_chat.id, text="Unkown command :(")
 
 start_handler = CommandHandler('start', start)
 stop_handler = CommandHandler('stop', stop)
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 token = '' #insert here your BotFather token
+user_id = '' #insert here your userid, if you don't know it leave blank and send anything to the bot, it will reply with your user id
 
 files = ["test"]
 softwares = ["notepad"]
